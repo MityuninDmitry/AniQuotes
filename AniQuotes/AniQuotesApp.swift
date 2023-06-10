5aa8fe6841ad3c96e4ac21aa8ce6a0fe53a6cffb
Mityunin Dmitry <mity-ml@mail.ru>
2023-06-10 21:13:05 +0500

    Initial Commit

---
+++ AniQuotes/AniQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniQuotesApp.swift
+//  AniQuotes
+//
+//  Created by Dmitry Mityunin on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AniQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
